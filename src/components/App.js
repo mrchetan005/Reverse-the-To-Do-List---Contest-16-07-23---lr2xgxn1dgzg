916fd51b0f1ace7e6ba689bb5ce9982090f9ec36
Chetan <mr.chetan005@gmail.com>
2023-07-16 18:26:17 +0530

    updated key

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,13 @@ import React, { useState } from 'react'
 import '../styles/App.css';
 
 function ToDo(props) {
-  const { id, createdAt} = props;
+  const { id, createdAt, inputValue } = props;
   return (<tr>
     <td>
       <p>{id}</p>
     </td>
     <td>
-      <input />
+      <input value={inputValue} />
     </td>
     <td>
       <p>{createdAt}</p>
@@ -33,11 +33,11 @@ function App() {
       }}>Reverse</button>
       <table>
         <tbody>
-    {
-      todos.map((todo)=>(
-        <ToDo id={todo.id} createdAt={todo.createdAt}/>
-      ))
-    }
+          {
+            todos.map((todo) => (
+              <ToDo key={todo.id} id={todo.id} createdAt={todo.createdAt} />
+            ))
+          }
         </tbody>
       </table>
     </div>
